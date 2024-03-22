1cf3f5371020b75133181731d41974a8fbd8309a
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-03-23 00:23:48 +0600

    Added task management

--- src/redux/features/tasks/tasksSlice.js
+++ src/redux/features/tasks/tasksSlice.js
@@ -1,7 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    tasks: [],
+    tasks: [
+        {
+            id: 1,
+            status: 'pending',
+            title: "Remove Button",
+            description: 'This is a description',
+            date: '22-03-2024',
+            assignedTo: 'Mir Hussain',
+            priority: 'high'
+        }
+    ],
 };
 
 const tasksSlice = createSlice({
@@ -9,16 +19,25 @@ const tasksSlice = createSlice({
     initialState,
     reducers: {
         addTask: (state, { payload }) => {
-            if (state.tasks.length === 0) {
-                state.tasks.push({ id: 1, ...payload });
+            if (state.tasks.length == 0) {
+                state.tasks.push({ id: 1, status: 'pending', ...payload });
             } else {
                 const lastElement = state.tasks.at(-1);
-                state.tasks.push({ id: lastElement.id + 1, ...payload });
+                state.tasks.push({ id: lastElement.id + 1, status: 'pending', ...payload });
             }
+        },
+
+        removeTask: (state, { payload }) => {
+            state.tasks = state.tasks.filter((item) => item.id !== payload);
+        },
+
+        updateStatus: (state, { payload }) => {
+            const target = state.tasks.find((item) => item.id == payload.id);
+            target.status = payload.status;
         }
     },
 });
 
-export const { addTask } = tasksSlice.actions;
+export const { addTask, updateStatus, removeTask } = tasksSlice.actions;
 
 export default tasksSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksSlice from "./features/tasks/tasksSlice";
+import UserSlice from "./features/user/UserSlice";
 
 const store = configureStore({
     reducer: {
         tasksSlice: tasksSlice,
+        userSlice: UserSlice,
     },
 });
 
